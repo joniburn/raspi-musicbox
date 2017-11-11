909234c2d91456a1bff0f78a04f9d3b593c58157
joniburn <joniburn@gmail.com>
2017-11-12 01:49:25 +0900

    標準入力から2バイトずつ読み込む処理を実装

--- toneserver.c
+++ toneserver.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 
 #include <limits.h>
 #include <errno.h>
 #include <argp.h>
+
+#include <sys/timerfd.h>
+#include <sys/epoll.h>
+#include <arpa/inet.h>
+
+#define MAX_EVENTS 2
 
 typedef struct {
   int outpin;
@@ -76,11 +84,81 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state) {
 }
 
 int main(int argc, char *argv[]) {
+  int ret;
+
   // 引数パース
   options opt;
   struct argp argp = {option_definitions, parse_opt, "", "", NULL, NULL, NULL};
   argp_parse(&argp, argc, argv, 0, NULL, &opt);
 
   printf("outpin=%d\n", opt.outpin);
+
+  // タイマー初期化
+  int timerfd = timerfd_create(CLOCK_MONOTONIC, TFD_NONBLOCK);
+  if (timerfd == -1) {
+    perror("timerfd_create");
+    return EXIT_FAILURE;
+  }
+
+  // メインループ処理のためのepoll初期化
+  int epollfd = epoll_create1(0);
+  if (epollfd == -1) {
+    perror("epoll_create1");
+    return EXIT_FAILURE;
+  }
+  struct epoll_event ev1 = {EPOLLIN, {0}};
+  ev1.data.fd = STDIN_FILENO;
+  ret = epoll_ctl(epollfd, EPOLL_CTL_ADD, STDIN_FILENO, &ev1);
+  if (ret == -1) {
+    perror("epoll_ctl");
+    return EXIT_FAILURE;
+  }
+  struct epoll_event ev2 = {EPOLLIN, {0}};
+  ev1.data.fd = timerfd;
+  ret = epoll_ctl(epollfd, EPOLL_CTL_ADD, timerfd, &ev2);
+  if (ret == -1) {
+    perror("epoll_ctl");
+    return EXIT_FAILURE;
+  }
+
+  // メインループ
+  uint8_t stdin_buf[2] = {0, 0};
+  int stdin_read_bytes = 0;
+  while (1) {
+    struct epoll_event events[MAX_EVENTS];
+    int nfds;
+
+    nfds = epoll_wait(epollfd, events, MAX_EVENTS, -1);
+    if (ret == -1) {
+      perror("epoll_wait");
+      return EXIT_FAILURE;
+    }
+    for (int i = 0; i < nfds; i++) {
+      int fd = events[i].data.fd;
+      if (fd == timerfd) {
+        // do timer stuff
+        printf("DEBUG: timer fire\n");
+      } else if (fd == STDIN_FILENO) {
+        // read stdin
+        ssize_t read_bytes = read(STDIN_FILENO,
+                                  stdin_buf + stdin_read_bytes,
+                                  sizeof(stdin_buf) - stdin_read_bytes);
+        if (read_bytes == -1) {
+          perror("read");
+          return EXIT_FAILURE;
+        }
+        stdin_read_bytes += read_bytes;
+        printf("DEBUG: stdin_read_bytes=%d, stdin_buf=[%u, %u]\n", stdin_read_bytes, stdin_buf[0], stdin_buf[1]);
+
+        if (stdin_read_bytes >= 2) {
+          uint16_t in = *((uint16_t *) stdin_buf);
+          in = ntohs(in);
+          printf("DEBUG: in=%u\n", in);
+          stdin_read_bytes = 0;
+        }
+      }
+    }
+  }
+
   return EXIT_SUCCESS;
 }
