3062c5e848b86b46c6d62cef29657a727ab3a3ed
joniburn <joniburn@gmail.com>
2017-11-12 10:10:22 +0900

    refactoring

--- toneserver/toneserver.c
+++ toneserver/toneserver.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <assert.h>
 #include <sys/timerfd.h>
 #include <sys/epoll.h>
 #include <arpa/inet.h>
@@ -10,42 +11,19 @@
 
 #define MAX_EVENTS 2
 
-int main(int argc, char *argv[]) {
-  int ret;
+static int initialize_timer();
+static int initialize_epoll(int timerfd);
 
+int main(int argc, char *argv[]) {
   // 引数パース
   options opt;
   parse_args(argc, argv, &opt);
 
   printf("outpin=%d\n", opt.outpin);
 
-  // タイマー初期化
-  int timerfd = timerfd_create(CLOCK_MONOTONIC, TFD_NONBLOCK);
-  if (timerfd == -1) {
-    perror("timerfd_create");
-    return EXIT_FAILURE;
-  }
-
-  // メインループ処理のためのepoll初期化
-  int epollfd = epoll_create1(0);
-  if (epollfd == -1) {
-    perror("epoll_create1");
-    return EXIT_FAILURE;
-  }
-  struct epoll_event ev1 = {EPOLLIN, {0}};
-  ev1.data.fd = STDIN_FILENO;
-  ret = epoll_ctl(epollfd, EPOLL_CTL_ADD, STDIN_FILENO, &ev1);
-  if (ret == -1) {
-    perror("epoll_ctl");
-    return EXIT_FAILURE;
-  }
-  struct epoll_event ev2 = {EPOLLIN, {0}};
-  ev1.data.fd = timerfd;
-  ret = epoll_ctl(epollfd, EPOLL_CTL_ADD, timerfd, &ev2);
-  if (ret == -1) {
-    perror("epoll_ctl");
-    return EXIT_FAILURE;
-  }
+  // ファイルディスクリプタの初期化
+  int timerfd = initialize_timer();
+  int epollfd = initialize_epoll(timerfd);
 
   // メインループ
   uint8_t stdin_buf[2] = {0, 0};
@@ -55,7 +33,8 @@ int main(int argc, char *argv[]) {
     int nfds;
 
     nfds = epoll_wait(epollfd, events, MAX_EVENTS, -1);
-    if (ret == -1) {
+    assert(nfds <= MAX_EVENTS);
+    if (nfds == -1) {
       perror("epoll_wait");
       return EXIT_FAILURE;
     }
@@ -75,7 +54,7 @@ int main(int argc, char *argv[]) {
         }
         if (read_bytes == 0) {
           // end of file
-          exit(EXIT_SUCCESS);
+          return EXIT_SUCCESS;
         }
         stdin_read_bytes += read_bytes;
         printf("DEBUG: stdin_read_bytes=%d, stdin_buf=[%u, %u]\n", stdin_read_bytes, stdin_buf[0], stdin_buf[1]);
@@ -91,4 +70,44 @@ int main(int argc, char *argv[]) {
   }
 
   return EXIT_SUCCESS;
+}
+
+// タイマー初期化
+static int initialize_timer() {
+  int timerfd = timerfd_create(CLOCK_MONOTONIC, TFD_NONBLOCK);
+  if (timerfd == -1) {
+    perror("timerfd_create");
+    exit(EXIT_FAILURE);
+  }
+  return timerfd;
+}
+
+// メインループ処理のためのepoll初期化
+static int initialize_epoll(int timerfd) {
+  int ret;
+
+  int epollfd = epoll_create1(0);
+  if (epollfd == -1) {
+    perror("epoll_create1");
+    exit(EXIT_FAILURE);
+  }
+
+  // 監視対象fdその1: 標準入力
+  struct epoll_event ev1 = {EPOLLIN, {0}};
+  ev1.data.fd = STDIN_FILENO;
+  ret = epoll_ctl(epollfd, EPOLL_CTL_ADD, STDIN_FILENO, &ev1);
+  if (ret == -1) {
+    perror("epoll_ctl");
+    exit(EXIT_FAILURE);
+  }
+
+  // 監視対象fdその2: タイマー
+  struct epoll_event ev2 = {EPOLLIN, {0}};
+  ev1.data.fd = timerfd;
+  ret = epoll_ctl(epollfd, EPOLL_CTL_ADD, timerfd, &ev2);
+  if (ret == -1) {
+    perror("epoll_ctl");
+    exit(EXIT_FAILURE);
+  }
+  return epollfd;
 }
