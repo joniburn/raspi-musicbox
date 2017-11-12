7bd56fd49940c63da95677be1a4b09e336cd156b
joniburn <joniburn@gmail.com>
2017-11-12 19:17:43 +0900

    テスト用にfloat型で周波数を出力するプログラムを作成

---
+++ toneserver/write_d.c
@@ -0,0 +1,17 @@
+// テスト用に レ の音の周波数を標準出力に書き込むプログラム
+
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+int main(void) {
+  float freq = 587.3295515;  // レの音の周波数
+  uint32_t buf;
+
+  memcpy(&buf, &freq, sizeof(float));
+  buf = htonl(buf);
+
+  write(1, &buf, sizeof(buf));
+
+  return 0;
+}
