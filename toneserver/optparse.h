5e332ae07614c48eb30bbbd05a067a8971d70d47
joniburn <joniburn@gmail.com>
2017-11-12 09:57:13 +0900

    引数パース処理を別ファイルに分離

---
+++ toneserver/optparse.h
@@ -0,0 +1,20 @@
+#ifndef OPTPARSE_H__
+
+// コマンドライン引数のパース結果を格納する構造体
+typedef struct {
+  int outpin;
+} options;
+
+/**
+ * コマンドライン引数をパースする。
+ *
+ * 引数に不備がある場合はエラーを出力してプログラムを終了する。
+ *
+ * @param argc mainに渡されるargc
+ * @param argv mainに渡されるargv
+ * @param opt  パース結果の格納先
+ */
+extern void parse_args(int argc, char **argv, options *opt);
+
+#define OPTPARSE_H__
+#endif /* OPTPARSE_H__ */
--- toneserver/toneserver.c
+++ toneserver/toneserver.c
@@ -2,94 +2,20 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-
-#include <limits.h>
-#include <errno.h>
-#include <argp.h>
-
 #include <sys/timerfd.h>
 #include <sys/epoll.h>
 #include <arpa/inet.h>
 
+#include "optparse.h"
+
 #define MAX_EVENTS 2
-
-typedef struct {
-  int outpin;
-} options;
-
-static struct argp_option option_definitions[] = {
-  { "outpin", 'o', "BCM", 0, "The output pin number(BCM).", 0 },
-  { 0 }
-};
-
-/**
- * 文字列を整数にパースする。
- *
- * @param in  入力文字列
- * @param out 結果の書き込み先
- * @return
- *   正しくパースできた場合、0。
- *   エラーの場合、1。
- */
-static int parse_int(const char *in, int *out) {
-  char *endptr;
-  int ret;
-
-  // 空文字列はエラー
-  if (*in == '\0') {
-    fprintf(stderr, "empty string could not be parsed into a number.\n");
-    return 1;
-  }
-
-  ret = strtol(in, &endptr, 10);
-
-  // 整数としてパースできなかった
-  if (*endptr != '\0') {
-    fprintf(stderr, "number parse error: [%s]\n", in);
-    return 1;
-  }
-
-  // 値が範囲外
-  if (errno == ERANGE || ret > INT_MAX || ret < INT_MIN) {
-    fprintf(stderr, "number range error: [%s]\n", in);
-    return 1;
-  }
-
-  *out = ret;
-  return 0;
-}
-
-static error_t parse_opt(int key, char *arg, struct argp_state *state) {
-  options *dest = state->input;
-
-  switch (key) {
-  case 'o': {
-    int value, ret;
-    ret = parse_int(arg, &value);
-    if (ret) {
-      argp_error(state, "failed to parse outpin: [%s]", arg);
-    }
-    if (value <= 0) {
-      argp_error(state, "pin number should be a positive integer: %d\n", value);
-    }
-    dest->outpin = value;
-    break;
-  }
-  case ARGP_KEY_ARG:
-    return 0;
-  default:
-    return ARGP_ERR_UNKNOWN;
-  }
-  return 0;
-}
 
 int main(int argc, char *argv[]) {
   int ret;
 
   // 引数パース
   options opt;
-  struct argp argp = {option_definitions, parse_opt, "", "", NULL, NULL, NULL};
-  argp_parse(&argp, argc, argv, 0, NULL, &opt);
+  parse_args(argc, argv, &opt);
 
   printf("outpin=%d\n", opt.outpin);
 
